5826b89516d3a26088409ea5d92dab47bdafdd3b
mike13k <mikegeorge2012@gmail.com>
2019-10-08 23:40:17 +0200

    finish implementation of applyOperators and remove HP from states because path cost is enough

--- src/EndGame.java
+++ src/EndGame.java
@@ -8,6 +8,7 @@ public class EndGame extends GeneralSearchProblem {
 	int [] thanosPos;
 	int gridWidth;
 	int gridHeight;
+	int numOfWorriors;
 	
 	/**
 	 * EndGame search tree consists of Nodes, just like any search tree;
@@ -21,16 +22,19 @@ public class EndGame extends GeneralSearchProblem {
 	 * 
 	 * therefore, our EndGame state contains:
 	 * 
-	 * iX, iY, HP,
+	 * iX, iY (Location of Iron Man)
 	 * (s1C)ollected, s2C., s3C., s4C., s5C., s6C.,
 	 * (w1K)illed, w2K, w3k, w4k, w5k	
+	 * 
+	 * The damage taken by Iron man at a certain node will be represented by 
+	 * the path cost to that node.
 	 * 
 	 * @param initialState
 	 */
 	public EndGame(int [] ironmanPos, int gridWidth, int gridHeight,
 			int [] worriorsPos, int [] stonesPos, int [] thanosPos) {
 		
-		super(createInitialState(ironmanPos));
+		super(createInitialState(ironmanPos, worriorsPos.length / 2));
 		String [] operators = new String []{
 				"up", "down", "left", "right", 
 				"collect", "kill", "snap"};
@@ -41,28 +45,26 @@ public class EndGame extends GeneralSearchProblem {
 		this.worriorsPos = worriorsPos;
 		this.stonesPos = stonesPos;
 		this.thanosPos = thanosPos;
+		this.numOfWorriors = worriorsPos.length / 2;
 	}
 	
-	private static Node createInitialState(int [] ironmanPos) {
+	private static Node createInitialState(int [] ironmanPos, int numOfWorriors) {
 		HashMap<String, Integer> state; Node parentNode;
 		int depth; int pathCost;
 
 		state = new HashMap<String, Integer>();
 		state.put("iX",ironmanPos[0]); state.put("iY",ironmanPos[1]);
-		state.put("hp",100); //HP
 		for(int i = 1; i <= 6; i++) {
 			state.put("s" + i + "C",0);
 		}
-		for(int i = 1; i <= 6; i++) {
+		for(int i = 1; i <= numOfWorriors; i++) {
 			state.put("w" + i + "K",0);
 		}
 		parentNode = null;
 		depth = 1;
 		pathCost = 0;
-
 		
 		return new Node(state, parentNode, null, depth, pathCost);		
-		
 	}
 	
 	@Override
@@ -88,10 +90,35 @@ public class EndGame extends GeneralSearchProblem {
 
 	public Node applyOperator(Node node, String operator) {
 		@SuppressWarnings("unchecked")
-		HashMap<String, Integer> nodeState = 
-				(HashMap<String, Integer>) node.getState().clone();
+		HashMap<String, Integer> nodeState = new HashMap<String, Integer>();		
+		HashMap<String, Integer> oldState = (HashMap<String, Integer>) node.getState();
+		
+		// Initialize node state with the previous state values
+		nodeState.put("iX", oldState.get("iX")); nodeState.put("iY", oldState.get("iY"));
+		for(int i = 1; i <= 6; i++) {
+			nodeState.put("s" + i + "C", oldState.get("s" + i + "C"));
+//			System.out.println(nodeState.get("s" + i + "C"));
+		}
+		for(int i = 1; i <= numOfWorriors; i++) {
+			nodeState.put("w" + i + "K", oldState.get("w" + i + "K"));
+//			System.out.println(nodeState.get("s" + i + "C"));
+		}
+		
+//		System.out.println(operator);
+//		System.out.println(nodeState.get("iX"));
+//		System.out.println(nodeState.get("iY"));
+//		System.out.println(node.getPathCost());
+//		System.out.println(node.getDepth());
+//		System.out.println("--");
+
+
 		int cost = node.getPathCost();
 		int depth = node.getDepth() + 1;
+		
+		// TODO is this allowed?
+		if(cost > 100) {
+			return null;
+		}
 
 		Integer newValue;
 		switch(operator) {
@@ -99,8 +126,15 @@ public class EndGame extends GeneralSearchProblem {
 			newValue = (Integer) nodeState.get("iX");
 			newValue--;
 			if ( newValue >= 0) {
-				nodeState.put("iX", newValue);
-				cost++;
+				for(int i = 1; i <= this.numOfWorriors; i++) {
+					if(nodeState.get("w" + i + "K") == 0) {
+						if( (this.worriorsPos[(i-1) * 2] == newValue) &&
+							(this.worriorsPos[((i-1) * 2) + 1] == nodeState.get("iY"))) {
+							return null;
+						}
+					}
+				}
+				nodeState.put("iX", newValue);				
 			} else {
 				return null;
 			}
@@ -109,9 +143,16 @@ public class EndGame extends GeneralSearchProblem {
 		case "down":
 			newValue = (Integer) nodeState.get("iX");
 			newValue++;
-			if ( newValue <= gridHeight) {
+			if ( newValue < gridHeight) {
+				for(int i = 1; i <= this.numOfWorriors; i++) {
+					if(nodeState.get("w" + i + "K") == 0) {
+						if( (this.worriorsPos[(i-1) * 2] == newValue) &&
+							(this.worriorsPos[((i-1) * 2) + 1] == nodeState.get("iY"))) {
+							return null;
+						}
+					}
+				}
 				nodeState.put("iX", newValue);
-				cost++;
 			} else {
 				return null;
 			}
@@ -121,8 +162,15 @@ public class EndGame extends GeneralSearchProblem {
 			newValue = (Integer) nodeState.get("iY");
 			newValue--;
 			if ( newValue >= 0) {
+				for(int i = 1; i <= this.numOfWorriors; i++) {
+					if(nodeState.get("w" + i + "K") == 0) {
+						if( (this.worriorsPos[(i-1) * 2] == nodeState.get("iX")) &&
+							(this.worriorsPos[((i-1) * 2) + 1] == newValue)) {
+							return null;
+						}
+					}
+				}
 				nodeState.put("iY", newValue);
-				cost++;
 			} else {
 				return null;
 			}
@@ -131,9 +179,16 @@ public class EndGame extends GeneralSearchProblem {
 		case "right": 
 			newValue = (Integer) nodeState.get("iY");
 			newValue++;
-			if ( newValue <= gridWidth) {
+			if ( newValue < gridWidth) {
+				for(int i = 1; i <= this.numOfWorriors; i++) {
+					if(nodeState.get("w" + i + "K") == 0) {
+						if( (this.worriorsPos[(i-1) * 2] == nodeState.get("iX")) &&
+							(this.worriorsPos[((i-1) * 2) + 1] == newValue)) {
+							return null;
+						}
+					}
+				}
 				nodeState.put("iY", newValue);
-				cost++;
 			} else {
 				return null;
 			}
@@ -141,18 +196,119 @@ public class EndGame extends GeneralSearchProblem {
 			
 		case "collect": 
 			// collecting stone decreases HP by 3
-			cost += 3;
+			for(int i = 1; i <= 6; i++) {
+				if( nodeState.get("s" + i + "C") == 0 && 
+					nodeState.get("iX") == this.stonesPos[(i-1) * 2] &&
+					nodeState.get("iY") == this.stonesPos[((i-1) * 2) + 1]) {
+					cost += 3;
+					nodeState.put("s" + i + "C", 1);
+					break;
+				} else if(i == 6) {
+					return null;
+				}
+			}
 			
 			break;
 		case "kill":
 			// killing warrior decreases HP by 2
-			cost += 2;
+			boolean isWorriorFound = false;
+			
+			for(int i = 1; i <= this.numOfWorriors; i++) {
+				if(nodeState.get("w" + i + "K") == 0) {
+					if( (this.worriorsPos[(i-1) * 2] - nodeState.get("iX") == 1) ||
+						(this.worriorsPos[(i-1) * 2] - nodeState.get("iX") == -1)) {
+						
+						if(this.worriorsPos[((i-1) * 2) + 1] == nodeState.get("iY")) {
+							isWorriorFound = true;
+							cost += 2;
+							nodeState.put("w" + i + "K", 1);
+						}
+						
+					} else if(this.worriorsPos[(i-1) * 2] == nodeState.get("iX")) {
+						
+						if( (this.worriorsPos[((i-1) * 2) + 1] - nodeState.get("iY") == 1) || 
+							(this.worriorsPos[((i-1) * 2) + 1] - nodeState.get("iY") == -1)) {
+							
+							isWorriorFound = true;
+							cost += 2;
+							nodeState.put("w" + i + "K", 1);
+							
+						}
+						
+					}
+				}
+			}
+			
+			if(!isWorriorFound) {
+				return null;
+			}
 
 			break;
-		case "snap": break;
-		}
-		// thanos hits ironman by 5 hp
+		case "snap":
+			if(this.thanosPos[0] == nodeState.get("iX") &&
+			  this.thanosPos[1] == nodeState.get("iY")) {
+			
+				return new Node(nodeState, node, operator, depth, cost); 
 				
+			} else {
+				return null;
+			}
+			
+		}
+		
+		// warriors hits ironman by 1 hp each
+		for(int i = 1; i <= this.numOfWorriors; i++) {
+			if(nodeState.get("w" + i + "K") == 0) {
+				if( (this.worriorsPos[(i-1) * 2] - nodeState.get("iX") == 1) ||
+					(this.worriorsPos[(i-1) * 2] - nodeState.get("iX") == -1)) {
+					
+					if(this.worriorsPos[((i-1) * 2) + 1] == nodeState.get("iY")) {
+						
+						cost++;
+						
+					}
+					
+				} else if(this.worriorsPos[(i-1) * 2] == nodeState.get("iX")) {
+					
+					if( (this.worriorsPos[((i-1) * 2) + 1] - nodeState.get("iY") == 1) || 
+						(this.worriorsPos[((i-1) * 2) + 1] - nodeState.get("iY") == -1)) {
+						
+						cost++;
+						
+					}
+					
+				}
+			}
+		}
+		
+		// thanos hits ironman by 5 hp
+		if( (this.thanosPos[0] - nodeState.get("iX") == 1) || 
+			(this.thanosPos[0] - nodeState.get("iX") == -1)) {
+			
+			if(this.thanosPos[1] == nodeState.get("iY")) {
+				
+				cost += 5;
+				
+			}
+				
+		} else if(this.thanosPos[0] == nodeState.get("iX")) {
+			
+			if( (this.thanosPos[1] - nodeState.get("iY") == 1) ||
+				(this.thanosPos[1] - nodeState.get("iY") == -1)) {
+				
+				cost += 5;
+				
+			}
+			
+		} else if(this.thanosPos[0] == nodeState.get("iX") &&
+				  this.thanosPos[1] == nodeState.get("iY")) {
+				
+			cost += 5;
+			
+		}
+		
+//		System.out.println("Pass");
+
 		return new Node(nodeState, node, operator, depth, cost);
 	}
 	
@@ -182,10 +338,10 @@ public class EndGame extends GeneralSearchProblem {
 		Node parentNode = node.getParentNode();
 		
 		if(parentNode == null) {
-			return true;
+			return false;
 		}
 		else if(parentNode.getState().equals(state)) {
-			return false;
+			return true;
 		} else {
 			return isRepeatedState(parentNode, state);
 		}
